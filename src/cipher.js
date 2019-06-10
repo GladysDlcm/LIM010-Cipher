a7d8c95ab7b9c30c00a52ba88e5bcd6eafd92535
GladysDlcm <gladysdlcmerello@gmail.com>
2019-06-10 10:21:10 -0500

    Cifra mayusculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,34 @@
 window.cipher = {
   encode: (offset, string) => {
-    /* Acá va tu código que cifra*/
+    
+    string=string.toUpperCase();
+    // string="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // offset=33;
+
+    let resultado = '';
+    
+    for(let i = 0; i < string.length; i++) {
+      let nuevoCaracter = (( string.charCodeAt(i) - 65 + offset) % 26 + 65);
+    //console.log(string.charCodeAt(i) + " - " + string.charAt(i) + " - " + nuevoCaracter + " - " + String.fromCharCode(nuevoCaracter));
+      resultado = resultado + String.fromCharCode(nuevoCaracter);
+    };
+
+    return resultado;
   },
   decode: (offset, string) => {
-    /* Acá va tu código que descifra*/
+
+    string=string.toUpperCase();
+    // string="HIJKLMNOPQRSTUVWXYZABCDEFG";
+    // offset=33;
+
+    let resultado = '';
+    
+    for(let i = 0; i < string.length; i++) {
+      let nuevoCaracter = (( string.charCodeAt(i) + 65 - offset) % 26 + 65);
+    //console.log(string.charCodeAt(i) + " - " + string.charAt(i) + " - " + nuevoCaracter + " - " + String.fromCharCode(nuevoCaracter));
+      resultado = resultado + String.fromCharCode(nuevoCaracter);
+    };
+
+    return resultado;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,33 +1,69 @@
 //Variables globales
 const usuario = document.getElementById('usuario');
-const btn_enviar = document.getElementById('btn_enviar');
+const password = document.getElementById('password');
+const btnEnviar = document.getElementById('btn-enviar');
+const btnCifrar = document.getElementById('cifrar');
+const btnDecifrar = document.getElementById('decifrar');
+const btnBorrar = document.getElementById('borrar');
+const btnCerrar = document.getElementById('cerrar');
 let intentos = 0;
 
+
 //Funcionalidad Login
-btn_enviar.addEventListener('click', () => {
+btnEnviar.addEventListener('click', () => {
 const nombre = usuario.value;
-const pass_ingreso = document.getElementById('password').value;
+const passIngreso = password.value;
 
     if (intentos == 2) {        //Error 3er intento
-        document.getElementById("password").disabled = true;
-        document.getElementById("usuario").disabled = true;
-        document.getElementById("btn_enviar").disabled = true;
-        document.getElementById("error").innerHTML= "Ya utilizaste todos tus intentos, en este momento no podrás ingresar.";
+        document.getElementById('password').disabled = true;
+        document.getElementById('usuario').disabled = true;
+        document.getElementById('btn-enviar').disabled = true;
+        document.getElementById('error').innerHTML= 'Ya utilizaste todos tus intentos, en este momento no podrás ingresar.';
     }
     else {      //Contraseña correcta ingresar
-        if (pass_ingreso == "LABORATORIA") {   
-            document.getElementById("hola_usuario").innerHTML = "Bienvenido " + nombre + ", los datos que ingresaras serán confidenciales.";
-            document.getElementById('pantalla_login').classList.add('hide');
-            document.getElementById('pantalla_cifrado').classList.remove('hide');
+        if (passIngreso == 'LABORATORIA') {   
+            document.getElementById('hola-usuario').innerHTML = 'Bienvenido ' + nombre + ', los datos que ingresaras serán confidenciales.';
+            document.getElementById('pantalla-login').classList.add('hide');
+            document.getElementById('pantalla-cifrado').classList.remove('hide');
         }
         else {
             if (intentos == 0){      //Error 1er intento
-                document.getElementById("error").innerHTML = "La contraseña es incorrecta.";
+                document.getElementById('error').innerHTML = 'La contraseña es incorrecta.';
             }
-            if (intentos == 1){      //Error 2er intento   
-                document.getElementById("error").innerHTML = "La contraseña es incorrecta, intente de nuevo.";
+            if (intentos == 1){      //Error 2er intento   '
+                document.getElementById('error').innerHTML = 'La contraseña es incorrecta, intente de nuevo.';
             } 
         }  
         intentos++;  //Cuenta intentos
     }  
 });
+
+//Boton cifrar
+btnCifrar.addEventListener('click', () => {
+    let numeroMes = document.getElementById('offset').value;
+    let datosPaciente = document.getElementById('datos').value;
+    document.getElementById('resultado').innerHTML = cipher.encode(parseInt(numeroMes), datosPaciente);
+    document.getElementById('mensaje').innerHTML= 'El código se ha generado con exito.';
+});
+
+//Boton decifrar
+btnDecifrar.addEventListener('click', () => {
+    let numeroMes = document.getElementById('offset').value;
+    let datosPaciente = document.getElementById('datos').value;
+    document.getElementById('resultado').innerHTML = cipher.decode(parseInt(numeroMes), datosPaciente);
+    document.getElementById('mensaje').innerHTML= 'El código se ha generado con exito.';
+});
+
+//Boton borrar
+btnBorrar.addEventListener('click', () => {
+    document.getElementById('resultado').innerHTML = '';
+    document.getElementById('mensaje').innerHTML= ''; 
+});
+
+//Boton cerrar
+btnCerrar.addEventListener('click', () => {
+    location.reload(true);
+});
+
+
+
